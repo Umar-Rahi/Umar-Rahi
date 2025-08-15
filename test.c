27e22d3e93edf0a5bc40d2e11f279de21fab0abc
Umar-Rahi <urahi788@gmail.com>
2025-08-15 09:39:30 +0530

    4

---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter any two numbers");
+    scanf("%d%d",&a,&b);
+    int c=a+b;
+    printf("/n sum of a and b id %d",c);
+    return  0;
+}
